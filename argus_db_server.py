870964b16b56497db6fe0b983b65b4cb9a39b991
Matthew Stagg <mstagg@cphandheld.com>
2015-07-28 12:59:11 -0500

    Query individual truck ids

--- argus_db_server.py
+++ argus_db_server.py
@@ -10,43 +10,62 @@ db = MySQLdb.connect(host="localhost",  # your host
                      user="root",       # username
                      passwd="Banshee8396",     # password
                      db="argus")   # name of the database
-#MySQLdb.cursors.DictCursor
-cursor = db.cursor()
 
 @app.route('/create/session', methods=['GET'])
 def new_session():
 	try:
+		cursor = db.cursor()
 		now = datetime.datetime.now()
 		time = now.isoformat()
 		cursor.execute("INSERT INTO sessions(session_key, date) Values (NULL, '" + time + "')")
 		db.commit()
+		cursor.close()
 		return "1"
 	except:
 		return "0"
 
-@app.route('/log/position/<string:id>/<string:status>/<string:xpos>/<string:ypos>/<string:zpos>', methods=['GET'])
-def log_pos(id, status, xpos, ypos, zpos):
-	try:
-		now = datetime.datetime.now()
-		time = now.isoformat()
-		cursor.execute("INSERT INTO positions(session_key, id, status, xpos, ypos, zpos, log_datetime) Values (last_insert_id(), '" + id + "', " + status + ", " + xpos + ", " + ypos + ", " + zpos + ", '" + time + "')")
-		db.commit()
-		return "1"
-	except:
-		return "0"
+@app.route('/log/position/<string:id>/<string:tick>/<string:status>/<string:xpos>/<string:ypos>/<string:zpos>', methods=['GET'])
+def log_pos(id, tick, status, xpos, ypos, zpos):
+	#try:
+	cursor = db.cursor()
+	now = datetime.datetime.now()
+	time = now.isoformat()
+	cursor.execute("INSERT INTO positions(session_key, id, tick, status, xpos, ypos, zpos, log_datetime) Values (last_insert_id(), '" + id + "', " + tick + ", " + status + ", " + xpos + ", " + ypos + ", " + zpos + ", '" + time + "')")
+	db.commit()
+	cursor.close()
+	return "1"
+	#except:
+	#	return "0"
 
-@app.route('/retrieve/positions/<string:session_key>', methods=['GET'])
-def get_pos(session_key):
+@app.route('/retrieve/positions/<string:session_key>/<string:t_id>', methods=['GET'])
+def get_pos(session_key, t_id):
 	try:
-		cursor.execute("SELECT * FROM positions WHERE session_key = " + session_key + " ORDER BY log_datetime ASC")
+		cursor = db.cursor()
+		cursor.execute("SELECT * FROM positions WHERE session_key = " + session_key + " AND id = '" + t_id + "' ORDER BY log_datetime ASC")
 		result = cursor.fetchall()
 		s = ""
 		for row in result:
-			s += str(row[0]) + "," + str(row[1]) + "," + str(row[3]) + "," + str(row[5]) + "<br/>"
+			s += str(row[0]) + "," + str(row[7]) + "," + str(row[1]) + "," + str(row[3]) + "," + str(row[5]) + "<br/>"
 		if(len(s) > 0):
+			cursor.close()
 			return s
 		else:
-			return "0";
+			cursor.close()
+			return "NONE";
+	except:
+		return "0"
+
+@app.route('/retrieve/length/<string:session_key>', methods=['GET'])
+def get_len(session_key):
+	try:
+		cursor = db.cursor()
+		cursor.execute("SELECT log_datetime FROM positions WHERE session_key = " + session_key + " ORDER BY log_datetime ASC LIMIT 1")
+		dateBegin = cursor.fetchone()[0]
+		cursor.execute("SELECT log_datetime FROM positions WHERE session_key = " + session_key + " ORDER BY log_datetime DESC LIMIT 1")
+		dateEnd = cursor.fetchone()[0]
+		len = str(dateEnd - dateBegin)
+		cursor.close()
+		return len
 	except:
 		return "0"
 
