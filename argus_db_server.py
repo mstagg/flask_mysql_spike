835cf9a74c005b8dd3fc7ac5de51a0754c4c5b08
Matthew Stagg <mstagg@cphandheld.com>
2015-07-31 12:08:59 -0500

    Refactoring

--- argus_db_server.py
+++ argus_db_server.py
@@ -6,11 +6,12 @@ import datetime
 
 app = Flask(__name__)
 cors = CORS(app)
-db = MySQLdb.connect(host="localhost",  # your host 
-                     user="root",       # username
-                     passwd="Banshee8396",     # password
-                     db="argus")   # name of the database
+db = MySQLdb.connect(host="localhost",  		# your host 
+                     user="root",       		# username
+                     passwd="Banshee8396",     	# password
+                     db="argus")   				# name of the database
 
+# Creates a new session to store truck positions
 @app.route('/create/session', methods=['GET'])
 def new_session():
 	try:
@@ -24,37 +25,41 @@ def new_session():
 	except:
 		return "0"
 
+# Logs relevant information regarding a trucks current position 
 @app.route('/log/position/<string:id>/<string:tick>/<string:status>/<string:xpos>/<string:ypos>/<string:zpos>', methods=['GET'])
 def log_pos(id, tick, status, xpos, ypos, zpos):
-	#try:
-	cursor = db.cursor()
-	now = datetime.datetime.now()
-	time = now.isoformat()
-	cursor.execute("INSERT INTO positions(session_key, id, tick, status, xpos, ypos, zpos, log_datetime) Values (last_insert_id(), '" + id + "', " + tick + ", " + status + ", " + xpos + ", " + ypos + ", " + zpos + ", '" + time + "')")
-	db.commit()
-	cursor.close()
-	return "1"
-	#except:
-	#	return "0"
+	try:
+		cursor = db.cursor()
+		now = datetime.datetime.now()
+		time = now.isoformat()
+		cursor.execute("INSERT INTO positions(session_key, id, tick, status, xpos, ypos, zpos, log_datetime) Values (last_insert_id(), '" + id + "', " + tick + ", " + status + ", " + xpos + ", " + ypos + ", " + zpos + ", '" + time + "')")
+		db.commit()
+		cursor.close()
+		return "1"
+	except:
+		return "0"
 
+# Returns a CSV formatted list of all logged truck positions for a given session ID and truck ID 
 @app.route('/retrieve/positions/<string:session_key>/<string:t_id>', methods=['GET'])
 def get_pos(session_key, t_id):
 	try:
 		cursor = db.cursor()
-		cursor.execute("SELECT * FROM positions WHERE session_key = " + session_key + " AND id = '" + t_id + "' ORDER BY log_datetime ASC")
+		cursor.execute("SELECT tick, xpos, zpos FROM positions WHERE session_key = " + session_key + " AND id = '" + t_id + "' ORDER BY log_datetime ASC")
 		result = cursor.fetchall()
 		s = ""
 		for row in result:
-			s += str(row[0]) + "," + str(row[7]) + "," + str(row[1]) + "," + str(row[3]) + "," + str(row[5]) + "<br/>"
+			s += str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "<br>"
 		if(len(s) > 0):
 			cursor.close()
+			s = s[0:len(s) - 4] # Remove last '<br>' from string 
 			return s
 		else:
 			cursor.close()
-			return "NONE";
+			return "0";
 	except:
 		return "0"
 
+# Returns the total length of time that a session ran for
 @app.route('/retrieve/length/<string:session_key>', methods=['GET'])
 def get_len(session_key):
 	try:
